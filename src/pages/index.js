00e18ef8226dce8623b06f276c5be2dcd33c9fb9
unknown <anders@kitson.org>
2018-12-08 09:34:54 -0700

    two thirds through 15

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import Listing from '../components/listing'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Listing />
   </Layout>
 )
 
